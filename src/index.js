9498f495fa9135e690abe026064f80d2df1d26c1
Javi Morales <javi@moralesf.com>
2018-11-13 17:56:10 +0100

    empty renderer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import './MoralesRenderer.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MoralesRenderer extends Component {
+    render() {
+        return (
+            <div className="App">
+                <ul className="App-list">
+                    Morales renderer!
+                </ul>
+            </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        );
+    }
+}
+
+export default MoralesRenderer;
